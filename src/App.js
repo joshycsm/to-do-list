3063f31b0f7e4bd066ddb944dc6ec991c7e2bc22
github username <github email address>
2020-03-26 16:38:02 -0600

    font awesome for react and basic functionality with css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ListItems from "./ListItems";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
+      }
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({
+      currentItem: {
+        text: event.target.value,
+        key: Date.now()
+      }
+    });
+  }
+
+  addItem(event) {
+    event.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if (newItem.text !== "") {
+      // destructuring assignment
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: ""
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <form action="" id="to-do-form" onSubmit={this.addItem}>
+          <input
+            type="text"
+            placeholder="Enter To Do Task"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}
+          />
+          <button type="submit">Add To Do</button>
+        </form>
+
+        <ListItems items={this.state.items} />
       </div>
     );
   }
